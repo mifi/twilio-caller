2e56676bb3e73e6de0ad04c9fca564b648e0404b
Mikael Finstad <finstaden@gmail.com>
2025-02-10 17:53:55 +0800

    updates

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 # twilio-caller
+
+Tool for calling using twilio API from the browser. Useful for testing. Uses `@twilio/voice-sdk`.
+
+In twilio create a new number and an app, e.g. `twilio-caller` and link it to the number. Also create an API key.
+
+Then create a Function in Twilio with path `/twilio-caller/handle-call` and set the app's Webhook (Request URL) to the full URL of the Function. This function will simply forward the call to `@twilio/voice-sdk` in the browser:
+
+```js
+const assert = require('node:assert');
+
+exports.handler = function(context, event, callback) {
+  const callerId = '+123456789'; // Update with your twilio number
+
+  // Only handle outgoing calls
+  assert(event.To !== callerId.replaceAll(/[^\d+]/g, ''));
+
+	const twiml = new Twilio.twiml.VoiceResponse();
+
+  // set the callerId (from)
+  let dial = twiml.dial({ callerId });
+
+  dial.number(event.To);
+
+  // This callback is what is returned in response to this function being invoked.
+  // It's really important! E.g. you might respond with TWiML here for a voice or SMS response.
+  // Or you might return JSON data to a studio flow. Don't forget it!
+  callback(null, twiml);
+};
+```
+
+Ref:
+- https://www.twilio.com/docs/serverless/functions-assets/functions/invocation
+- https://www.twilio.com/docs/voice/twiml/dial
+
+
+Setup `.env` with data from Twilio admin, then:
+
+```bash
+tsx makeAccessToken.ts
+
+yarn dev
+```
+
+and go to your browser
---
+++ makeAccessToken.ts
@@ -0,0 +1,41 @@
+import assert from 'node:assert';
+import 'dotenv/config'
+
+import { jwt } from 'twilio';
+
+// https://www.twilio.com/docs/iam/access-tokens#creating-tokens
+// https://www.twilio.com/en-us/blog/make-receive-phone-calls-browser-twilio-programmable-voice-python-javascript
+// function handler hosted here:
+// https://console.twilio.com/us1/develop/functions/editor/ZS83f6a234f7191bde5b2365e3e309cfd2/environment/ZE9328f7b4749661edceeb1976d1967c2e/function/ZH57f1e22b68f9bf1c4615a5014d6b6652
+
+// Used when generating any kind of tokens
+// To set up environmental variables, see http://twil.io/secure
+const twilioAccountSid = process.env.TWILIO_ACCOUNT_SID;
+const twilioApiKey = process.env.TWILIO_API_KEY;
+const twilioApiSecret = process.env.TWILIO_API_SECRET;
+const outgoingApplicationSid = process.env.TWIML_APP_SID;
+const identity = 'user';
+
+assert(twilioAccountSid);
+assert(twilioApiKey);
+assert(twilioApiSecret);
+assert(identity);
+
+// Create a "grant" which enables a client to use Voice as a given user
+const voiceGrant = new jwt.AccessToken.VoiceGrant({
+  outgoingApplicationSid: outgoingApplicationSid,
+  incomingAllow: true,//todo needed?
+});
+
+// Create an access token which we will sign and return to the client,
+// containing the grant we just created
+const token = new jwt.AccessToken(
+  twilioAccountSid,
+  twilioApiKey,
+  twilioApiSecret,
+  { identity },
+);
+token.addGrant(voiceGrant);
+
+// Serialize the token to a JWT string
+console.log(token.toJwt());
